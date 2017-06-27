844af3476428bb926a7344bcd51528fd3516cc1a
Ely Marrero <Ely.Marrero@teachforamerica.org>
2017-06-27 08:30:09 -0400

    After 6/26 Javascript

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,13 +3,82 @@ function say(something){
 }
 
 function makeRow(propertyName, value){
-    say("<TR><TD>"+propertyName+":</TD><TD>"+ value + "</TD></TR>");
+    if(Array.isArray(value)){
+        say("<TR><TD>" + propertyName + ":</TD><TD>" + value.toString().replace(/,/g, ", ") + "</TD></TR>");
+    }else {
+        say("<TR><TD>" + propertyName + ":</TD><TD>" + value + "</TD></TR>");
+    }
 }
 
-var hotel = {
-    name: "Marriott Long Wharf",
-    rooms: 300,
-    booked: 310,
-    gym: true,
-    roomTypes: ["suite", "single", "king", "waterfall"]
-};
+function getMonthName(monthNumber){
+    var months = ["January", "February", "March", "April", "May",
+    "June", "July", "August", "September", "October", "November",
+    "December"];
+
+    return months[monthNumber];
+}
+
+function Hotel(name, rooms, booked, gym, roomTypes){
+   this.name = name;
+   this.rooms = rooms;
+   this.booked = booked;
+   this.gym = gym;
+   this.roomTypes = roomTypes;
+   this.hasOpenings = function(){
+       return (this.booked < this.rooms);
+   }
+}
+
+var hotel4 = new Hotel(
+    "Fountainbleu Hilton",
+    800,
+    20,
+    true,
+    ["suite", "double", "single", "oceanview"]
+);
+
+var hotel1 = new Hotel(
+    "Marriott Long Wharf",
+    300,
+    310,
+    true,
+    ["suite", "single", "king", "waterfall"]
+);
+
+var hotel2 = new Hotel(
+    "Mohonk Mountain House",
+    200,
+    1,
+    true,
+    ["tower", "single"]
+);
+
+var hotel3 = new Hotel(
+    "Fairmont Mont Tremblant",
+    300,
+    30,
+    true,
+    ["suite", "high floor", "presidential suite", "royal suite"]
+);
+
+var allHotels = [hotel1, hotel2, hotel3, hotel4];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
