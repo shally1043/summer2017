e3469198d0bfb6cea39ee8493219382cc368034e
Ely Marrero <Ely.Marrero@teachforamerica.org>
2017-07-12 16:55:19 -0400

    last PHP

--- php/functions.php
+++ php/functions.php
@@ -130,7 +130,7 @@ does a pretty good job but sometimes not so good.</p>
     <label for="aFormat">Enter a PHP format string:</label>
     <input type="text" id="aFormat" name="aFormat" />
 
-    <input type="submit" name="action" value="formatdate" />
+    <input type="submit" name="action" value="formatdate" /></form>
 
     <?php if(isset($_GET['action']) && $_GET['action'] == 'formatdate') { ?>
         You typed: <?=$_GET['someDateTime']?> and the format: <?=$_GET['aFormat']?>
@@ -166,7 +166,211 @@ does a pretty good job but sometimes not so good.</p>
         <li> Raise to a power - pow(<?=pi()?>, 3) = <?=pow(pi(), 3)?></li>
         <li> Hypotenuse of a triangle given 2 legs - hypot(3,4) = <?=hypot(3,4)?></li>
     </ul>
+    </p>
+    <h4>Strings</h4>
+    <p>Return part of a string:
+        <?php $mystr = "The quick brown fox jumps over the lazy dog"; ?>
+        <strong><?=$mystr?></strong><br/>
+        <ul>
+        <li>substr($mystr, 0, 3) = <?=substr($mystr, 0, 3)?></li>
+        <li>substr($mystr, 4) = <?=substr($mystr, 4)?></li>
+        <li>substr($mystr, -3) = <?=substr($mystr, -3)?></li>
+        </ul>
+    </p>
+    <hr/>
+    <p>Replace part of a string:
+        <?php
+            $mystr2 = "The quick brown fox jumps over the lazy dog";
+            $mystr3 = "The quick brown fox jumps over the LAZY dog";
+            $replacements1 = 0;
+            $replacements2 = 0;
+            $mystr2 = str_replace("lazy", "cute", $mystr2, $replacements1);
+            $mystr3 = str_ireplace("lazy", "cute", $mystr3, $replacements2);
+        ?>
+        <strong><?=$mystr2?></strong><br/>
+        <ul>
+            <li>
+                str_replace("lazy", "cute", $mystr2) = <?=$mystr2?>
+                (made <?=$replacements1?> replacements)
+            </li>
+
+            <li>str_ireplace("lazy", "cute", $mystr3) = <?=$mystr3?>
+                (made <?=$replacements2?> replacements)</li>
+        </ul>
+    </p>
+    <p>ASCII Codes:
+        <form action="functions.php" method="get">
+            <label for="code">Enter a character code:</label>
+            <input type="text" id="code" name="code" />
+            <label for="char">Enter a character</label>
+            <input type="text" id="char" name="char" />
+            <input type="submit" name="action" value="ascii" />
+        </form>
+        <?php
+            if(isset($_GET['action']) && $_GET['action'] == 'ascii') {
+                if(isset($_GET['code'])){
+                    print "Character code ". $_GET['code']." is: ".chr($_GET['code'])."<br />";
+                }
+
+                if(isset($_GET['char'])){
+                    print "The code for character ". $_GET['char']." is: ".ord($_GET['char'])."<br />";
+                }
+            }
+        ?>
+
+        <hr/>
+    </p>
+    <p>Length of a string in characters
+        <strong>"this is our test string"</strong><br/>
+        <?=strlen("this is our test string")?>
+    </p><hr/>
+<p>
+    Length of a string in words:
+    <strong>"This is another test string"</strong><br />
+    <?=str_word_count("This is another test string")?>
+</p><hr>
+<p>Character count by char:
+    <strong>"This is yet another test string"</strong><br/>
+    <?=print_r(count_chars("This is yet another test string"))?>
+</p><hr />
+<p>Search a string:
+    <strong>Looking for the word "long" in "this is a very long string to search"</strong><br />
+    It is at position <?=strpos("this is a very long string to search", "long")?><br/>
+    <strong>Looking for the word "short" in "this is a very long string to search"</strong><br />
+    It is at position <?=strpos("this is a very long string to search", "short")?><br/>
+</p><hr />
+<p>Trim whitespace from a string:<br/>
+    <strong>"    this string contains leading and trailing whitespace    "</strong><br/>
+    "<?=trim("    this string contains leading and trailing whitespace    ")?>"
+
+</p><hr/>
+<p>Wrap your words...
+    <?php $theStory = "this is my short story and it is probably longer than I would like."; ?>
+    <strong>input: "<?=$theStory?>"</strong><br/>
+    <?=wordwrap($theStory, 3, "<br />")?><hr/>
+    <?=wordwrap($theStory, 3, "<br />", true)?>
+</p><hr />
+<p>Playing with case:
+    <strong>Input: <?=theStory?></strong>
+    <ul>
+        <li>strtoupper($theStory) = <?=strtoupper($theStory)?></li>
+        <li>strtolower($theStory) = <?=strtolower($theStory)?></li>
+        <li>ucfirst($theStory) = <?=ucfirst($theStory)?></li>
+        <li>ucwords($theStory) = <?=ucwords($theStory)?></li>
+        <li>str_replace(" ", "", ucwords($theStory)) = <?=str_replace(" ", "", ucwords($theStory))?></li>
+</ul>
+</p><hr/>
+<p>Hashing!<br/>
+    <?php $inputString = "this is the original input string on which we will calculate a sha1 hash";?>
+    <strong>Input: <?=$inputString?></strong><br />
+    The hash is: <?=sha1($inputString)?><br/>
+    <strong>Now we will add a period to the end...</strong>
+    <?php $inputString = $inputString."." ?>
+    <strong>The input is now: <?=$inputString?></strong><br />
+    ...and the hash is: <?=sha1($inputString)?>
+</p><br/><hr />
+<p>Automatically escaping a string:<br/>
+<pre>
+    <?php
+        $originalString = "I'm a lumberjack and I'm okay!";
+        $a = addslashes($originalString);
+        $b = addslashes($a);
+        $c = addslashes($b);
+        print "\$originalString = $originalString \n";
+        print "\$a = $a \n";
+        print "\$b = $b \n";
+        print "\$c = $c \n";
+    ?>
+
+</pre>
+
 </p>
+<p>Format some numbers:<br/>
+<ul>
+    <li>number_format(<?=pi()?>, 3) = <?=number_format(pi(), 3)?></li>
+    <li>number_format(<?=pi()?>, 3, ",", ".") = <?=number_format(pi(), 3, ",", ".")?></li>
+    <li>number_format(92960000.12345, 3) = <?=number_format(92960000.12345, 3)?></li>
+
+</ul></p>
+<hr/>
+<p>Strip tags:<br/>
+    <?php
+        $userinput = "<p>this is my paragraph <script src='badscript.js'></script></p><?php ?>";
+    ?>
+    <strong>Original input:</strong>
+    <pre>
+        <?=htmlspecialchars($userinput)?>
+    </pre><br />
+    <strong>After strippage:</strong>
+    <pre>
+        <?=htmlspecialchars(strip_tags($userinput, "<p><strong><br><hr>"))?>
+    </pre>
+
+
+</p><hr/>
+<p>One word after another:<br/>
+    <ul>
+        <li>Does &quot;bravo&quot; come before &quot;alpha&quot;? <?=strcmp("bravo", "alpha")?></li>
+        <li>Does &quot;alpha&quot; come before &quot;bravo&quot;? <?=strcmp("alpha", "bravo")?></li>
+        <li>Does &quot;alpha&quot; come before &quot;alpha&quot;? <?=strcmp("alpha", "alpha")?></li>
+        <li>Does &quot;Alpha&quot; come before &quot;alpha&quot;? <?=strcmp("Alpha", "alpha")?></li>
+</ul>
+
+
+</p><hr>
+<p>Padding a string:<br>
+<pre>
+    <?=str_pad("a", 21, ".", STR_PAD_BOTH)?> 1
+    <?=str_pad("aaa", 21, ".", STR_PAD_BOTH)?> 2
+    <?=str_pad("aaaaa", 21, ".", STR_PAD_BOTH)?> 3
+    <?=str_pad("aaaaaaa", 21, ".", STR_PAD_BOTH)?> 4
+    <?=str_pad("aaaaaaaaa", 21, ".", STR_PAD_BOTH)?> 5
+    <?=str_pad("aaaaaaaaaaa", 21, ".", STR_PAD_BOTH)?> 6
+    <?=str_pad("aaaaaaaaaaaaa", 21, ".", STR_PAD_BOTH)?> 7
+    <?=str_pad("aaaaaaaaaaaaaaa", 21, ".", STR_PAD_BOTH)?> 8
+    <?=str_pad("aaa", 21, ".", STR_PAD_BOTH)?> 2
+    <?=str_pad("aaa", 21, ".", STR_PAD_BOTH)?> 2
+
+</pre>
+</p><br />
+<p>Format a string:
+<?=printf("The Sun is %e miles away from %s", 92960000, "Earth")?>
+</p><br />
+<p>Parse a URL string:<br/>
+    <?php
+        $stringToBeParsed = "thing1=first&thing2=second&thing3=third";
+        parse_str($stringToBeParsed);
+    ?>
+    $stringToBeParsed = <?=$stringToBeParsed?><br/>
+    $thing1=<?=$thing1?><br/>
+    $thing2=<?=$thing2?><br />
+    $thing3=<?=$thing3?>
+</p><br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
 
 
 
